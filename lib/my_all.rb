6a6bbe1c2474d19a79d78d9aa1774f47e9c50170
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-04-30 11:42:44 -0400

    commit error

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def my_all?(collection)
-
+    all_vals = []
+    for i in 0..collection.length - 1
+        all_vals << yield(collection[i])
+    end
+    !all_vals.include?(false)
 end
