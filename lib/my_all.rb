5f18577ef3f198695edf1d75d811f7e3ba138eef
Pat McClernan <patmccler@gmail.com>
2020-04-30 10:00:42 -0400

    builds my_all method

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
+  counter = 0
+  return_value = true
 
+  while counter < collection.size
+    if !yield(collection[counter])
+      return_value = false
+    end
+    counter +=1
+  end
+  
+  return_value
 end
